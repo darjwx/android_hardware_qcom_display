25636183f9610aba0b8e4f99ea360199b6457205
Christian Oder <myself5@carbonrom.org>
2016-08-19 14:38:24 +0200

    Allow msm8226/msm8974 to use msm8994 HAL
    Thanks to @alviteri for the help
    
    Change-Id: If5639128440c7a184328ee8bf0c08196bd94a184

--- Android.mk
+++ Android.mk
@@ -6,10 +6,18 @@ ifneq ($(filter msm8084 msm8x84,$(TARGET_BOARD_PLATFORM)),)
 else
 ifneq ($(filter msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
     #This is for 8974 based (and B-family) platforms
+    ifeq ($(SONY_AOSP),true)
+    include $(call all-named-subdir-makefiles,msm8994)
+    else
     include $(call all-named-subdir-makefiles,msm8974)
+    endif
 else
 ifneq ($(filter msm8226 msm8x26,$(TARGET_BOARD_PLATFORM)),)
+    ifeq ($(SONY_AOSP),true)
+    include $(call all-named-subdir-makefiles,msm8994)
+    else
     include $(call all-named-subdir-makefiles,msm8226)
+    endif
 else
 ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,msm8960)
